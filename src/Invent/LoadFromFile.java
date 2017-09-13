f0148b6d5fb819e4e10db68487a5b2eb53cae2f9
greegav <greegav@gmail.com>
2017-09-13 17:07:15 +0300

    Всплывает окно выбора директории

--- src/Invent/LoadFromFile.java
+++ src/Invent/LoadFromFile.java
@@ -1,22 +1,49 @@
 package Invent;
 
+import javax.swing.*;
+import java.io.File;
+
 // Класс для загрузки данных из файла
-public class LoadFromFile {
+public class LoadFromFile extends JPanel {
     // TODO
     // Считать название директории с отчетами (получить путь)
     // В цикле пока есть файлы считывать инофрмацию из каждого следующего файла
     // и добавлять в коллекцию
     //
     public static String s1;
+    static JFileChooser fc;
+
+    public LoadFromFile() {
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);//do nothing
+    }
+
 
     public static String getDirName(){
+        fc = new JFileChooser();
+        //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = fc.showOpenDialog(LoadFromFile.fc);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+        }
         s1= "Запускаем окно выбора папки\n";
+
+        String path="";
+
+        if (path.length()>0) {
+
+            System.out.println(path);
+        } else
+        {
+            s1+="Path not found!\n";
+        }
+
         s1+= "Получаем путь\n\n";
 
         return s1;
     }
 
-    public static void loadFromFile (String path){
+    public static void loadFromFile(String path){
 
         path="Проверяем наличие файла\n";
         path+="Если файл есть - считываем его содержимое и добавляем в коллекцию\n";
