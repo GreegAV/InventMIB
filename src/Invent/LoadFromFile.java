c84f73c85a491c2c31af7b5a7a71c86872d34fdb
greegav <greegav@gmail.com>
2017-09-14 16:03:47 +0300

    Getting list of flies in folder

--- src/Invent/LoadFromFile.java
+++ src/Invent/LoadFromFile.java
@@ -2,6 +2,9 @@ package Invent;
 
 import javax.swing.*;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 // Класс для загрузки данных из файла
 public class LoadFromFile  {
@@ -13,7 +16,7 @@ public class LoadFromFile  {
     //
     static JFileChooser fc;
 
-
+// Получаем имя папки с файлами отчетов.
     private static String getDirName(){
         fc = new JFileChooser();
         String path=null;
@@ -26,13 +29,39 @@ public class LoadFromFile  {
         } else {
             System.exit(0);
         }
-
         return path;
     }
+// получаем список файлов в папке
 
+    private static List getListFiles(String path){
+        List<String> listOfFiles = new ArrayList<String>();
+
+
+        System.out.println("Получаем список файлов");
+        File files[] = new File(path).listFiles();
+//If this pathname does not denote a directory, then listFiles() returns null.
+        System.out.println("Получен список файлов");
+
+        for (File file: files) {
+            if (file.isFile()) {
+                listOfFiles.add(file.getName());
+            }
+        }
+        Collections.sort(listOfFiles);
+        for (String fileName:listOfFiles ){
+            System.out.println(fileName);
+        }
+        return listOfFiles;
+    }
+
+// загружаем данные из файла
     public static void loadFromFile(){
-
-        System.out.println(getDirName());
+        System.out.println("Получаем имя папки");
+        String path=getDirName();
+        System.out.println("Печатаем полученное имя папки");
+        System.out.println(path);
+        System.out.println("Получаем и печатаем список файлов");
+        getListFiles(path);
     }
 
 
