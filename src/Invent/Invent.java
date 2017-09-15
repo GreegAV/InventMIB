8bc5a80cb972f4de102341931bd0cd645f03e6e2
greegav <greegav@gmail.com>
2017-09-15 14:03:23 +0300

    Attempt 2. With Scanner...

--- src/Invent/Invent.java
+++ src/Invent/Invent.java
@@ -2,10 +2,12 @@ package Invent;
 // основной класс программы
 
 
+import java.io.FileNotFoundException;
+
 import static Invent.LoadFromFile.loadFromFile;
 
 public class Invent {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Инвентаризация!\n");
         loadFromFile();
 
