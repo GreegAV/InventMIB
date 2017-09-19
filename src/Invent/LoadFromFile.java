8f9c3c95cb0fa16fbcf8b9f2c55340e9bbc112b6
greegav <greegav@gmail.com>
2017-09-19 15:03:18 +0300

    Красивый рефактор регулярного парсинга строки

--- src/Invent/LoadFromFile.java
+++ src/Invent/LoadFromFile.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,7 +18,7 @@ class LoadFromFile {
     //
     private static JFileChooser fc;
     private static int numlines = 0;
-    private static int countXP=0, count7=0, count10=0;
+    private static int countXP = 0, count7 = 0, count10 = 0;
 
     // Получаем имя папки с файлами отчетов.
     static String getDirName() {
@@ -64,7 +65,9 @@ class LoadFromFile {
         for (String tmpFileName : fileList) {
             readFromFile(tmpFileName);
         }
-
+        System.out.println("XP - " + countXP);
+        System.out.println("7 - " + count7);
+        System.out.println("10 - " + count10);
     }
 
 
@@ -85,18 +88,19 @@ class LoadFromFile {
         String pcName = "";
         String osName = "";
         String osSP = "";
+        List<String> tokens2;
 
 
         while ((sc.hasNextLine() && (curLine != null))) {
             if (curLine.contains("Комп'ютер  ") || curLine.contains("Компьютер  ")) {
-                String[] tokensPCName = curLine.split(" ");
-                List<String> tokens2 = new ArrayList<>();
+                String[] tokens = curLine.split(" ");
+                tokens2 = new ArrayList<>((Arrays.asList(tokens)));
 
-                for (String t : tokensPCName) {
-                    if (t != " ") {
-                        tokens2.add(t);
-                    }
-                }
+//                for (String t : tokens) {
+//                    if (t != " ") {
+//                        tokens2.add(t);
+//                    }
+//                }
 
                 //TODO записать в элемент инвентаризации
                 pcName = tokens2.get(2).substring(1, tokens2.get(2).length());
@@ -104,41 +108,48 @@ class LoadFromFile {
                 curLine = sc.nextLine();
                 curLine = sc.nextLine();
                 if (curLine.contains("Операцiйна система") || curLine.contains("Операционная система")) {
-                    String[] tokensOS = curLine.split(" ");
-                    List<String> tokens2OS = new ArrayList<>();
+                    tokens2 = new ArrayList<>((Arrays.asList(curLine.split(" "))));
 
-                    int ii=0;
-                    for (String t : tokensOS) {
-                        if (t != " ") {
-                            tokens2OS.add(t);
-                            // System.out.println(ii+"-"+t);
-                            // ii++;
-                        }
-                    }
+//                    for (String t : tokensOS) {
+//                            tokens2OS.add(t);
+//                    }
 
                     //TODO записать в элемент инвентаризации
-                    osName=tokens2OS.get(5)+" "+tokens2OS.get(6).substring(0, 3);
-                    switch (tokens2OS.get(5)){
-                        case "XP":countXP++; break;
-                        case "7":count7++; break;
-                        case "10":count10++; break;
+                    osName = tokens2.get(5) + " " + tokens2.get(6).substring(0, 3);
+                    switch (tokens2.get(5)) {
+                        case "XP":
+                            countXP++;
+                            break;
+                        case "7":
+                            count7++;
+                            break;
+                        case "10":
+                            count10++;
+                            break;
                     }
-                    System.out.println(pcName+" "+osName);
-
-
+                    // TODO Форматированный вывод куда-то. Передавать, например в ReportGenerator.
+                    System.out.println(pcName + " " + osName);
                 }
-
-
             }
 
+
+            if (curLine.contains("Пакет обновления ОС") || curLine.contains("Пакет оновлення ОС")) {
+                System.out.println("Found !");
+                tokens2 = new ArrayList<>((Arrays.asList(curLine.split(" "))));
+
+//                for (String t : tokens) {
+//                        tokens2.add(t);
+//                }
+            }
+
+
+            // Считываем следующую строку ниже по файлу
             curLine = sc.nextLine();
 
         }
 
         sc.close();
-        System.out.println("XP - "+countXP);
-        System.out.println("7 - "+count7);
-        System.out.println("10 - "+count10);
+
     }
 
 
