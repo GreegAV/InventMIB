6de3353932c92dcc37ca1aa3730d662b8620d41d
greegav <greegav@gmail.com>
2017-09-20 14:34:54 +0300

    Попытки привязать в maven

--- src/Invent/ReportGenerator.java
+++
@@ -1,8 +0,0 @@
-package Invent;// класс генерации суммарного отчета
-
-public class ReportGenerator {
- //TODO подумать в каком виде оно должно быть
-    public void doReport(){
-        System.out.println("Генерируем отчет.");
-    }
-}
